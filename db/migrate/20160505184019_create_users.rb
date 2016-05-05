7524c0bd4f9caaec001c6f925c84d8654ceb13d9
smicl11 <bryan.smith094@gmail.com>
2016-05-05 11:43:49 -0700

    added user model

---
+++ db/migrate/20160505184019_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :current_city
+
+      t.timestamps null: false
+    end
+  end
+end
