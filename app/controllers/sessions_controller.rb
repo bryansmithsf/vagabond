fdf60fc75e5210c23d270a7a2ebedf6a413d5f46
Herman Chan <hermchan89@gmail.com>
2016-05-06 10:36:35 -0700

    added styling, partials, gems

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user_params = params.require(:user).permit(:email, :password)
     @user = User.confirm(user_params)
     if @user
       login(@user)
