419feece6f2d12d33195812eeac93fcf128884f8
scoobaroo <scoobaroo415@gmail.com>
2016-05-06 10:45:34 -0700

    half finished paperclip add

--- Gemfile
+++ Gemfile
@@ -25,7 +25,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-
+gem 'dotenv-rails', :groups => [:development, :test]
+gem 'paperclip'
+gem 'aws-sdk'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505220858) do
+ActiveRecord::Schema.define(version: 20160506163740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,10 +30,14 @@ ActiveRecord::Schema.define(version: 20160505220858) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "current_city"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_foreign_key "posts", "users"
