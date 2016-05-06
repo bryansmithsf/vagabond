6b3928194a89ba17a2d6f9fae23ae3f57d0c7546
scoobaroo <scoobaroo415@gmail.com>
2016-05-06 11:01:32 -0700

    created city model and controller

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,52 @@
+class CitiesController < ApplicationController
+  def index
+    @cities = City.all
+    render :index
+  end
+  def new
+    if current_user
+      @city = City.new
+      render :new
+    else
+      flash[:notice]="You are not authorized to create cities!"
+      redirect_to cities_path
+    end
+  end
+  def create
+    @city = City.create(city_params)
+    redirect_to cities_path
+  end
+  def show
+    @city = City.find(params[:city_id])
+    render :show
+  end
+  def edit
+    if current_user
+      @city = City.find(params[:city_id])
+      render :edit
+    else
+      flash[:notice]="You are not authorized to edit cities!"
+      redirect_to cities_path
+    end
+  end
+  def update
+    @city = City.find(params[:city_id])
+    @city.update(city_params)
+    flash[:notice]="City Succesfully Updated!"
+    redirect_to city_path(@city)
+  end
+  def destroy
+    @city = City.find(params[:city_id])
+    if current_user
+      @city.destroy
+      redirect_to cities_path
+    else
+      flash[:notice]="You are not authorized to delete cities!"
+      redirect_to cities_path
+    end
+  end
+  private
+  def post_params
+    params.require(:city).permit(:name,:image_url)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506163740) do
+ActiveRecord::Schema.define(version: 20160506175555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
