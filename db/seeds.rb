243be7f66566db168950df2b1a9127718adba3c0
smicl11 <bryan.smith094@gmail.com>
2016-05-07 14:11:05 -0700

    seed data for users and cities created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create( email: "test@test.com", password_digest: "123", current_city: "London", first_name: "Bob", last_name: "Smith")
+puts "users created"
+
+City.create( name: "San Francisco", image: "http://www.sanfrancisco.travel/sites/sftraveldev.prod.acquia-sites.com/files/field/image/site-photo.jpg" )
+City.create( name: "London", image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/London_from_a_hot_air_balloon.jpg")
+puts "Cities created"
