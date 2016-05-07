8c52822c9f29c25aa9217e86b9b9edfa8f58c476
smicl11 <bryan.smith094@gmail.com>
2016-05-07 16:36:27 -0700

    added new city to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,5 @@ puts "users created"
 
 City.create( name: "San Francisco", image: "http://www.sanfrancisco.travel/sites/sftraveldev.prod.acquia-sites.com/files/field/image/site-photo.jpg" )
 City.create( name: "London", image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/London_from_a_hot_air_balloon.jpg")
-City.create( name: "Gibraltar", image: "")
+City.create( name: "Gibraltar", image: "https://engineerontheroad.files.wordpress.com/2013/04/dscf1593.jpg")
 puts "Cities created"
