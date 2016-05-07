e28d64d69b73d8bf474a87a5cb927a187c387d67
ahza64 <ahza64@yahoo.com>
2016-05-07 13:03:00 -0700

    small syntax updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
+
   def index
     @users = User.all
     @posts =  Post.all.order(created_at: :desc)
     render :index
   end
+
   def new
     if current_user
       @post = Post.new
@@ -13,6 +15,7 @@ class PostsController < ApplicationController
       redirect_to posts_path
     end
   end
+
   def create
     @user = current_user
     @post = Post.create(post_params)
@@ -20,11 +23,13 @@ class PostsController < ApplicationController
     @user.posts.order(created_at: :desc)
     redirect_to posts_path
   end
+
   def show
     @post = Post.find(params[:post_id])
     @user = @post.user
     render :show
   end
+
   def edit
     if current_user
       @post = Post.find(params[:post_id])
@@ -34,23 +39,27 @@ class PostsController < ApplicationController
       redirect_to posts_path
     end
   end
+
   def update
     @post = Post.find(params[:post_id])
     @post.update(post_params)
     flash[:notice]="Post Succesfully Updated!"
     redirect_to post_path(@post)
   end
+
   def destroy
     @post = Post.find(params[:post_id])
     if current_user
       @post.destroy
       redirect_to posts_path
     else
-      flash[:notice]="You are not authorized to delete libraries!"
+      flash[:notice]="You are not authorized to delete Posts!"
       redirect_to posts_path
     end
   end
+
   private
+  
   def post_params
     params.require(:post).permit(:title,:text)
   end
