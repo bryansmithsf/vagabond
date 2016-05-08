31adc741f81c98565d16be5502fbe691d61a374d
scoobaroo <scoobaroo415@gmail.com>
2016-05-07 22:29:18 -0700

    added button stylings

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,3 @@
 class City < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, dependent: :destroy
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  belongs_to :city
+  belongs_to :city, dependent: :destroy
   validates :title, length: { minimum: 1 }
   validates :title, length: { maximum: 200 }
   validates :text, length: { minimum: 1 }
